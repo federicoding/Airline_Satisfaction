b67832e14c4e41ef680c621dcde84515c0e8312f
federicoding <63846374+federicoding@users.noreply.github.com>
2021-04-30 16:21:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,22 +31,21 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True) 
       
     # following lines create boxes in which user can enter data required to make prediction
-     
-     Inflight_Wifi_Service = st.selectbox('Inflight Wifi Service',(1,2,3,4,5))
-     Ease_Of_Online_Booking = st.selectbox('Ease Of Online Booking',(1,2,3,4,5))
-     Food_And_Drink = st.selectbox('Food And Drink',(1,2,3,4,5))
-     Online_Boarding  = st.selectbox('Online Boarding',(1,2,3,4,5))
-     Seat_Comfort = st.selectbox('Seat Comfort',(1,2,3,4,5))
-     Inflight_Entertainment = st.selectbox('Inflight Entertainment',(1,2,3,4,5))
-     On_board_Service = st.selectbox('On-board Service',(1,2,3,4,5))
-     Leg_Room = st.selectbox('Leg Room',(1,2,3,4,5))
-     Baggage_Handling = st.selectbox('Baggage Handling',(1,2,3,4,5))
-     Checkin_Service = st.selectbox('Checkin Service',(1,2,3,4,5))
-     Inflight_Service = st.selectbox('Inflight Service',(1,2,3,4,5))
-     Cleanliness = st.selectbox('Cleanliness',(1,2,3,4,5))
-     Customer_Type_Returning_Customer = st.selectbox('Customer Type_Returning Customer',(0,1))
-     Type_Of_Travel_Personal_Travel = st.selectbox('Type Of Travel_Personal Travel',(0,1))
-     Class_Economy = st.selectbox('Class_Economy',(0,1))
+    Inflight_Wifi_Service = st.selectbox('Inflight Wifi Service',(1,2,3,4,5))
+    Ease_Of_Online_Booking = st.selectbox('Ease Of Online Booking',(1,2,3,4,5))
+    Food_And_Drink = st.selectbox('Food And Drink',(1,2,3,4,5))
+    Online_Boarding  = st.selectbox('Online Boarding',(1,2,3,4,5))
+    Seat_Comfort = st.selectbox('Seat Comfort',(1,2,3,4,5))
+    Inflight_Entertainment = st.selectbox('Inflight Entertainment',(1,2,3,4,5))
+    On_board_Service = st.selectbox('On-board Service',(1,2,3,4,5))
+    Leg_Room = st.selectbox('Leg Room',(1,2,3,4,5))
+    Baggage_Handling = st.selectbox('Baggage Handling',(1,2,3,4,5))
+    Checkin_Service = st.selectbox('Checkin Service',(1,2,3,4,5))
+    Inflight_Service = st.selectbox('Inflight Service',(1,2,3,4,5))
+    Cleanliness = st.selectbox('Cleanliness',(1,2,3,4,5))
+    Customer_Type_Returning_Customer = st.selectbox('Customer Type_Returning Customer',(0,1))
+    Type_Of_Travel_Personal_Travel = st.selectbox('Type Of Travel_Personal Travel',(0,1))
+    Class_Economy = st.selectbox('Class_Economy',(0,1))
     
     result =""
       
